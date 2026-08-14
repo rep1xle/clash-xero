a4219b8935e4f9093d5fc57dc98b9bb5233bef65
rep1xle <waferp1xle@gmail.com>
2026-08-14 16:48:31 +0530

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,14 @@
+// Paste the config object from your Firebase project here.
+// Firebase console -> Project settings -> General -> "Your apps" -> SDK setup and configuration.
+// These values are meant to be public in client-side code -- that's normal for Firebase.
+const firebaseConfig = {
+  apiKey: "AIzaSyC5gTOIknj4MCg7bjby4dhCFHljV2uEh0Y",
+  authDomain: "clash-xero.firebaseapp.com",
+  databaseURL: "https://clash-xero-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "clash-xero",
+  storageBucket: "clash-xero.firebasestorage.app",
+  messagingSenderId: "904873734722",
+  appId: "1:904873734722:web:6619b07ea22c4c76af73db"
+};
+
+firebase.initializeApp(firebaseConfig);
